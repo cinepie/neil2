6014e4761ca807da6ce7645e9621f98f80d327dc
ahmedneil2 <91541696+ahmedneil2@users.noreply.github.com>
2025-04-07 12:29:20 +0600

    Update info.py

--- info.py
+++ info.py
@@ -198,7 +198,7 @@ else:
     ON_HEROKU = False
 # Multiple streaming URLs through Cloudflare
 STREAM_URLS = [
-    "https://odd-darkness-074fsadsafsafasfjlknmmlkaytr9pe8afnhdklnfalskdftgy.bdmovieshub.workers.dev/",
+    "https://first100.bdmovieshub.workers.dev/",
     "https://your-cloudflare-domain-2.workers.dev/"
 ]
 # Default URL if no specific one is chosen
